91e17d5f66d94ac6e328b52738ba2d18d9828044
Vitor Carv <grupovcsis@gmail.com>
2023-11-02 10:07:17 -0300

    Ajuste dos calculos
    Ajuste dos calculos da loja de tinta

--- lojatintas.php
+++ lojatintas.php
@@ -20,15 +20,7 @@
 
     <p>Olá, estou disponibilizando este sistema online para lojas de tintas com o objetivo de calcular a quantidade de tinta por metros quadrados, você possa usar gratuitamente em caso de melhorias por favor envie para <a href="mailto:programador@vitorcarvalho.dev.br">programador@vitorcarvalho.dev.br</a><br/></p>
     
-    <!-- 
-
-    Manual
-
-    Considere que a cobertura da tinta é de 1 litro para cada 6 metros quadrados e que a tinta é vendida em latas de 18 litros , que custam R$ 80,00
-
-    Informe ao usuário as quantidades de tinta a serem compradas
-
-    1 litro pinta = 6 metros
+    <!-- 1 litro pinta = 6 metros
     18L pinta - 108 m2
     1,6 pinta - 21,6 m2
 
@@ -37,9 +29,32 @@
 
     Tinta
     33,3 / 18L = 1,85 latas (arredondar para cima = 2) 2 * R$80 = 160
-    33,3 / 3,6 = 9,25 latas (arredondar para cima = 10) 10 * R$25 = 250
+    33,3 / 3,6 = 9,25 latas (arredondar para cima = 10) 10 * R$25 = 250 -->
 
-     -->
+    <?php 
+
+    $MTdaArea = $_POST['MTdaarea'] ?? null;
+    $vrdaLata = $_POST['vrdalata'] ?? null;
+    $ltdaLata = $_POST['ltdalata'] ?? null;
+    $quantDemao = $_POST['quantDemao'] ?? null;
+
+    $PorcErro = 10;
+
+    if($MTdaArea == 0.0){
+        $QuantLatas = 0;
+    } else{
+        $QuantLatas = $MTdaArea/$ltdaLata;
+        $NumLatas = (int) $QuantLatas; // Transformou o numero em inteiro ao inves de quebrado
+        $QuantLatas10 = $NumLatas+($NumLatas / 100 * $PorcErro.'%');
+
+        $NumLatasFinal = (int)$QuantLatas10;
+        
+        #$valorTotal = $NumLatasFinal*$vrdaLata;
+        #$valorTotalComDemaos = $valorTotal*$quantDemao;  
+        #echo $valorTotalComDemaos;
+    }
+
+    ?>
     
 <p><a href="https://www.php.net/manual/en/index.php" target="_blank">Manual do PHP</a> | <a href="index.php">Voltar para o Início</a></p>
 
@@ -55,7 +70,7 @@
         </form>
 
         <div id="faixa-resultado"  style="text-align:center; color:#fff; margin:20px auto;">
-            É nessessário XX latas para pintar esta área<br/> Incluso 10% a mais que o calculado de tinta para ter uma sobra<br/>Valor total: XXX
+            É nessessário <?php echo $NumLatasFinal; ?> latas para pintar esta área<br/> Incluso 10% a mais que o calculado de tinta para ter uma sobra<br/>Valor total: 
         </div>
     </div>    
 </div>
